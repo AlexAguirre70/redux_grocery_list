808e62b47b7343fa8e220cef942d6713c4ffe651
Alex Aguirre <teamaguirre@gmail.com>
2022-09-21 21:18:49 -0700

    created the reducer, actions and subcription

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ const initialState = {
 const groceryReducer = (state=initialState, action)=>{
     switch(action.type){
         case 'grocery/add':
-            return[...state,
+            return[
+                ...state,
             {
-                text:action.text
+                text: action.text
             }
             ]
         case 'grocery/clear' :
@@ -22,6 +23,45 @@ const groceryReducer = (state=initialState, action)=>{
         default:
             return state
 
-        }
-        
+        }       
 }
+let store= Redux.createStore(groceryReducer)
+
+// clear Action
+const  clearList = ()=> {
+    document.getElementById('newItem').value =''
+    store.dispatch({
+        type:'grocery/clear'
+    })
+}
+//add Grocery item Action
+const newGrocery = (e)=>{
+    e.preventDefault()
+    let groceryText = document.getElementById('newItem').value
+    store.dispatch({
+        type:'grocery/add',
+        text: groceryText
+    })
+    console.log(store.getState())
+}
+
+// create the grocery list
+const renderList = (state)=>{
+    state.forEach(grocery => {
+        let li= document.createElement('li')
+        list.appendChild(li)
+        li.textContent=grocery.text
+    })
+}
+
+//pass the array to render the list
+const render =()=> {
+    const state=store.getState()
+    renderList(state)
+}
+// this subscribes to update on change of state
+store.subscribe(render)
+
+// adding the event listeners
+grocerySubmit.addEventListener('click',(e)=>{newGrocery(e)})
+clearBtn.addEventListener('click',clearList)
